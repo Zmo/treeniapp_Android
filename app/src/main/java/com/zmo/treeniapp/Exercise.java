cfad08a0bd5df3f95c6da7a96594faf56cccf434
Simo Kolppo <simo.kolppo@gmail.com>
2014-03-19 00:55:16 +0200

    Small changes

--- app/src/main/java/com/zmo/treeniapp/Exercise.java
+++ app/src/main/java/com/zmo/treeniapp/Exercise.java
@@ -11,9 +11,41 @@ public class Exercise {
     private int oneRepMax;
 
     public Exercise(String name) {
+        this.setName(name);
+        setNormalSetWeight(0);
+        setHeavySetWeight(0);
+        setOneRepMax(0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        normalSetWeight = 0;
-        heavySetWeight = 0;
-        oneRepMax = 0;
+    }
+
+    public int getNormalSetWeight() {
+        return normalSetWeight;
+    }
+
+    public void setNormalSetWeight(int normalSetWeight) {
+        this.normalSetWeight = normalSetWeight;
+    }
+
+    public int getHeavySetWeight() {
+        return heavySetWeight;
+    }
+
+    public void setHeavySetWeight(int heavySetWeight) {
+        this.heavySetWeight = heavySetWeight;
+    }
+
+    public int getOneRepMax() {
+        return oneRepMax;
+    }
+
+    public void setOneRepMax(int oneRepMax) {
+        this.oneRepMax = oneRepMax;
     }
 }
